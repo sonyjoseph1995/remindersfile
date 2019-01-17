f0cac6101ce8febc2111f35eca416978c2d903fc
sonyjoseph1995 <42372150+sonyjoseph1995@users.noreply.github.com>
2019-01-17 12:39:30 +0530

    notify message

---
+++ reminder/NotifyMessage.java
@@ -0,0 +1,14 @@
+package com.example.ashir.reminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotifyMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_message);
+
+    }
+}
